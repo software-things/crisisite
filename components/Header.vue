bdc1e6e0597dc0ade3aa0ffefd2ae7987932b7fe
Igor Podlawski <igor.podlawski@gmail.com>
2020-03-19 22:18:12 +0100

    header init

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,17 @@
 <template>
-<div class="header">
-  <div class="row">
-    <div class="small-12">
-      Header
+  <header class="header">
+    <div class="row header__row">
+      <div class="columns small-12 large-2">
+        <img src="~/assets/img/city-logo.svg" alt="Logo Katowice z napisem Katowice – dla odmiany">
+      </div>
+      <div class="columns small-12 large-7">
+        <p class="header__page-title">Wydział Zarządzania Kryzysowego Urzędu Miasta Katowice</p>
+      </div>
+      <div class="colums small-12 large-3">
+        <button type="button">
+          <img src="~/assets/img/icons/search.svg" alt="Wyszukaj w serwisie">
+        </button>
+      </div>
     </div>
-  </div>
-</div>
+  </header>
 </template>
