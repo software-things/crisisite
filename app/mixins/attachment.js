ff55748a53b071130cfa6fc64fac478709a25d49
Kamil Jedrkiewicz <k.jedrkiewicz@softwarethings.pro>
2020-03-26 14:30:19 +0100

    Poprawki

--- app/mixins/attachment.js
+++ app/mixins/attachment.js
@@ -1,42 +1,50 @@
 export default {
+  methods: {
+    async fileSize(url) {
+      return await this.$axios
+        .head("https://cors-anywhere.herokuapp.com/" + url)
+        .then(response => {
+          return response.headers["content-length"];
+        });
+    },
+    humanFileSize(size) {
+      const i = Math.floor(Math.log(size) / Math.log(1024));
+      return (
+        (size / Math.pow(1024, i)).toFixed(2) * 1 +
+        " " +
+        ["B", "kB", "MB", "GB", "TB"][i]
+      );
+    }
+  },
   mounted() {
     if (process.client) {
-      const fileSize = async url => {
-        return await this.$axios
-          .head("https://cors-anywhere.herokuapp.com/" + url)
-          .then(response => {
-            return response.headers["content-length"];
-          });
-      };
-      const humanFileSize = size => {
-        const i = Math.floor(Math.log(size) / Math.log(1024));
-        return (
-          (size / Math.pow(1024, i)).toFixed(2) * 1 +
-          " " +
-          ["B", "kB", "MB", "GB", "TB"][i]
-        );
-      };
       const files = document.querySelectorAll(".wp-block-file");
-      for (const file of files) {
+      files.forEach(file => {
         file.classList = "attachment";
+        const wrapper = document.createElement("div");
         const button = file.querySelector(".wp-block-file__button");
         const url = file.querySelector("a");
-        url.dataset.details = `Trwa pobieranie danych...`;
+        const details = document.createElement("div");
+        details.className = "attachment__details";
+        details.innerText = `Trwa pobieranie danych...`;
+        url.parentNode.insertBefore(wrapper, url);
+        wrapper.appendChild(url);
+        wrapper.appendChild(details);
         if (url) {
           const href = url.getAttribute("href");
           const extension = href
             .split(".")
             .pop()
             .toUpperCase();
-          fileSize(url).then(size => {
-            size = humanFileSize(size);
-            url.dataset.details = `Format pliku: ${extension} | Rozmiar: ${size}`;
+          this.fileSize(url).then(size => {
+            size = this.humanFileSize(size);
+            details.innerText = `Format pliku: ${extension} | Rozmiar: ${size}`;
           });
         }
         if (button) {
           button.classList = "attachment__button";
         }
-      }
+      });
     }
   }
 }
